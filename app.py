844e9ba3dbf84a2c0355a58e5e00d805734b6ca9
Emmanuel <Cadafi5112@gmail.com>
2025-11-30 03:22:30 -0400

    fix: Forzando re-ejecucion del workflow CI/CD despues de actualizar secrets

--- app.py
+++ app.py
@@ -10,3 +10,4 @@ if __name__ == '__main__':
     # El contenedor correrá en el puerto 8080 por defecto
     port = int(os.environ.get('PORT', 8080))
     app.run(debug=True, host='0.0.0.0', port=port)
+
